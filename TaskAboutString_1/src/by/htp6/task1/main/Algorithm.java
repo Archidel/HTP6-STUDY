5cb59cecbe74209cfb85584a08c3778566c016e8
Archidel <Archangel.albert.6@gmail.com>
2016-11-20 22:39:05 +0300

    refract

--- TaskAboutString_1/src/by/htp6/task1/main/Algorithm.java
+++ TaskAboutString_1/src/by/htp6/task1/main/Algorithm.java
@@ -5,11 +5,9 @@ import by.htp6.task1.io.Print;
 public class Algorithm {
 	
 	public void algorithm(String word, char [][] array){	
-	
 		for(int i = 0; i < array.length; i++){
 			for(int j = 0; j < array[i].length-1; j++){	
 				if(word.charAt(0) == array[i][j]){	//находим первый символ который совпал
-					
 					if(j <= array.length - 1){	// а вдруг след. символ для проверки за границей массива
 						if(word.charAt(1) == array[i][j + 1]){	//находим второе совпадение со вторым символом
 							if(deepCheck(array, i, j, 'h', word.toCharArray())){	//Проверяем координаты слова более тщательней
@@ -19,7 +17,6 @@ public class Algorithm {
 							}
 						}
 					}
-					
 					if( i <= array[i].length-1){
 						if(word.charAt(1) == array[i+1][j]){
 							if(deepCheck(array, i, j, 'v', word.toCharArray())){
@@ -29,9 +26,7 @@ public class Algorithm {
 							}
 						}	
 					}
-				
 				}
-				
 			}
 		}
 	}
@@ -39,7 +34,6 @@ public class Algorithm {
 	private boolean deepCheck(char[][] array, int i, int j, char direction, char[] wordChar){ 
 		int lenghtWordChar = wordChar.length;		
 		boolean findWord = false;
-		
 		if('h' == direction){
 			if((j + lenghtWordChar) <= array.length){
 				if(array[i][j + lenghtWordChar-1] == wordChar[lenghtWordChar - 1]){	//сравниваем последий символ заданного слова и ячейки массива где условно должен находиться последний символ
@@ -47,7 +41,6 @@ public class Algorithm {
 				}
 			}
 		}
-	
 		if('v' == direction){
 			if((i + lenghtWordChar) <= array.length){
 				if(array[i + lenghtWordChar-1][j] == wordChar[lenghtWordChar - 1]){ 
@@ -55,9 +48,6 @@ public class Algorithm {
 				}	
 			}
 		}
-	
 		return findWord;
-	}
-
-	
+	}	
 }
