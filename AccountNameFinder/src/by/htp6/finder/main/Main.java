b05a8317af4abfd7446fac3023b37dbc369a2059
Archidel <Archangel.albert.6@gmail.com>
2016-11-20 22:18:32 +0300

    fix

--- AccountNameFinder/src/by/htp6/finder/main/Main.java
+++ AccountNameFinder/src/by/htp6/finder/main/Main.java
@@ -3,8 +3,10 @@ package by.htp6.finder.main;
 import by.htp6.finder.findAccountName.FindAccountName;
 
 public class Main {
+	
 	public static void main(String [] args){
 		FindAccountName fan = new FindAccountName();
 		fan.finder();
 	}
+	
 }
